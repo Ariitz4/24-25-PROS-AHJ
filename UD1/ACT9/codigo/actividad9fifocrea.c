3d486d2b3e691143e9545ee03f5cadff540c75c4
Aritz <herreroaritz@gmail.com>
2024-10-10 13:56:41 +0200

    Añado los ejercicois 8 y 9 intento1 definitivo

---
+++ UD1/ACT9/codigo/actividad9fifocrea.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+int main (void)
+{
+	int fp;
+	int p, bytesleidos;
+	char saludo[] = "Un saludo !!!!!\n", buffer [10];
+	while (1) {
+		fp = open ("FIFO2", 0);
+		bytesleidos = read(fp, buffer, 1);
+		printf("Obteniendo información..."); 
+		while (bytesleidos != 0) {
+			printf("%s", buffer);
+			bytesleidos = read (fp, buffer, 1); // leo otro byte
+	}
+	close (fp);
+}
+return(0);
+}
